c346e781785ce22fed30f8c479e2c7da1048a67f
Mahmoud-Abdel-Moaz <mahmoudmoaz55@gmail.com>
2022-03-24 00:46:27 +0200

    finish Push Notification using firebase

--- android/app/src/main/kotlin/com/appgain/movieapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/appgain/movieapp/MainActivity.kt
@@ -1,70 +1,71 @@
 package com.appgain.movieapp
 
-/*import android.content.BroadcastReceiver
-import android.os.Bundle
+
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import androidx.annotation.NonNull
-
+import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.EventChannel.EventSink
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugins.GeneratedPluginRegistrant*/
-import io.flutter.embedding.android.FlutterActivity
-class MainActivity: FlutterActivity() {
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-    /* private val CHANNEL = "poc.deeplink.flutter.dev/channel"
-     private val EVENTS = "poc.deeplink.flutter.dev/events"
-     private var startString: String? = null
-     private var linksReceiver: BroadcastReceiver? = null
+class MainActivity : FlutterActivity() {
 
-     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-         registerWith(flutterEngine)
+    private val CHANNEL = "movieapptask.appgain.movieapp/channel"
+    private val EVENTS = "movieapptask.appgain.movieapp/events"
+    private var startString: String? = null
+    private var linksReceiver: BroadcastReceiver? = null
 
-         MethodChannel(flutterEngine.dartExecutor, CHANNEL).setMethodCallHandler { call, result ->
-             if (call.method == "initialLink") {
-                 if (startString != null) {
-                     result.success(startString)
-                 }
-             }
-         }
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
 
-         EventChannel(flutterEngine.dartExecutor, EVENTS).setStreamHandler(
-                 object : EventChannel.StreamHandler {
-                     override fun onListen(args: Any?, events: EventSink) {
-                         linksReceiver = createChangeReceiver(events)
-                     }
+        MethodChannel(flutterEngine.dartExecutor, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "initialLink") {
+                if (startString != null) {
+                    result.success(startString)
+                }
+            }
+        }
 
-                     override fun onCancel(args: Any?) {
-                         linksReceiver = null
-                     }
-                 }
-         )
-     }
+        EventChannel(flutterEngine.dartExecutor, EVENTS).setStreamHandler(
+                object : EventChannel.StreamHandler {
+                    override fun onListen(args: Any?, events: EventSink) {
+                        linksReceiver = createChangeReceiver(events)
+                    }
 
-     override fun onCreate(savedInstanceState: Bundle?) {
-         super.onCreate(savedInstanceState)
+                    override fun onCancel(args: Any?) {
+                        linksReceiver = null
+                    }
+                }
+        )
+    }
 
-         val intent = getIntent()
-         startString = intent.data?.toString()
-     }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-     override fun onNewIntent(intent: Intent) {
-         super.onNewIntent(intent)
-         if (intent.action === Intent.ACTION_VIEW) {
-             linksReceiver?.onReceive(this.applicationContext, intent)
-         }
-     }
+        val intent = getIntent()
+        startString = intent.data?.toString()
+    }
 
-     fun createChangeReceiver(events: EventSink): BroadcastReceiver? {
-         return object : BroadcastReceiver() {
-             override fun onReceive(context: Context, intent: Intent) { // NOTE: assuming intent.getAction() is Intent.ACTION_VIEW
-                 val dataString = intent.dataString ?:
-                 events.error("UNAVAILABLE", "Link unavailable", null)
-                 events.success(dataString)
-             }
-         }
-     }*/
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        if (intent.action === Intent.ACTION_VIEW) {
+            linksReceiver?.onReceive(this.applicationContext, intent)
+        }
+    }
+
+    fun createChangeReceiver(events: EventSink): BroadcastReceiver? {
+        return object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) { // NOTE: assuming intent.getAction() is Intent.ACTION_VIEW
+                val dataString = intent.dataString
+                        ?: events.error("UNAVAILABLE", "Link unavailable", null)
+                events.success(dataString)
+            }
+        }
+    }
 
 }
